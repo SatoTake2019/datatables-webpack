4a64f7ed0334dfe6047307e6ebb515d5c9112895
SatoTake2019 <kiri90hari7352@gmail.com>
2023-01-12 15:07:51 +0900

    add one option to DataTable init

--- myapps/static/js/__datatables__/initTable.js
+++ myapps/static/js/__datatables__/initTable.js
@@ -1,6 +1,6 @@
 import DataTable from "datatables.net-bs5";
 
 export default function initTable() {
-    let table = new DataTable('#example');
+    let table = new DataTable('#example', { paging: false });
     return table;
 }
